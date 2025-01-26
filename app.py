1d177f4d59b45726686e608ba3f8f7309615d478
Pawan Parihar <pawan@yali.ai>
2025-01-26 19:49:09 +0530

    added flask api app to expost a route to predict using model file generated

---
+++ app.py
@@ -0,0 +1,18 @@
+import pickle
+from flask import Flask, request, jsonify
+
+# Load the model
+with open("models/iris_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    features = data["features"]
+    prediction = model.predict([features])
+    return jsonify({"prediction": prediction.tolist()})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
