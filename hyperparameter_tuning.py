09ba25a543e2ba0af4e701a2a4914506b901acb8
Pawan Parihar <pawan@yali.ai>
2025-01-26 19:33:07 +0530

    Fixed linting issues

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+
 def objective(trial):
     n_estimators = trial.suggest_int("n_estimators", 50, 200)
     max_depth = trial.suggest_int("max_depth", 3, 20)
@@ -11,7 +12,12 @@ def objective(trial):
 
     iris = load_iris()
     X, y = iris.data, iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
 
     # Train model
     model = RandomForestClassifier(
@@ -21,12 +27,13 @@ def objective(trial):
         random_state=42
     )
     model.fit(X_train, y_train)
-    
+
     # Evaluate accuracy
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
 
     return acc
+
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
