492eabfa2e9204f12ae8529906c470fb489951dc
Pawan Parihar <pawan@yali.ai>
2025-01-26 20:00:12 +0530

    Fixed linting issues

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ with open("models/iris_model.pkl", "rb") as f:
 
 app = Flask(__name__)
 
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
     features = data["features"]
     prediction = model.predict([features])
     return jsonify({"prediction": prediction.tolist()})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
