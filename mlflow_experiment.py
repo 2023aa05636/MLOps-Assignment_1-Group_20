894ea1ead30ef141f35003d9be6516b98cbb45d3
Pawan Parihar <pawan@yali.ai>
2025-01-27 15:13:27 +0530

    updated mlflow_experiment.py to run experiments for 3 different models

--- mlflow_experiment.py
+++ mlflow_experiment.py
@@ -1,9 +1,12 @@
 import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_iris
+#from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.metrics import classification_report
 
 iris = load_iris()
 X, y = iris.data, iris.target
@@ -14,27 +17,64 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
+## Track Experiments
+models = [
+    (
+        "Logistic Regression", 
+        {"C": 1, "solver": 'liblinear'},
+        LogisticRegression(), 
+        (X_train, y_train),
+        (X_test, y_test)
+    ),
+    (
+        "Random Forest", 
+        {"n_estimators": 30, "max_depth": 3},
+        RandomForestClassifier(), 
+        (X_train, y_train),
+        (X_test, y_test)
+    ),
+    (
+        "XGBClassifier",
+        {"use_label_encoder": False, "eval_metric": 'logloss'},
+        XGBClassifier(), 
+        (X_train, y_train),
+        (X_test, y_test)
+    )
+]
+
+reports = []
+
+for model_name, params, model, train_set, test_set in models:
+    X_train = train_set[0]
+    y_train = train_set[1]
+    X_test = test_set[0]
+    y_test = test_set[1]
+    
+    model.set_params(**params)
+    model.fit(X_train, y_train)
+    
+    y_pred = model.predict(X_test)
+    report = classification_report(y_test, y_pred, output_dict=True)
+    reports.append(report)
+
 # MLflow experiment
 mlflow.set_experiment("Iris Classifier Experiment")
-
-with mlflow.start_run():
-    n_estimators = 300
-    max_depth = 20
-    model = RandomForestClassifier(
-        n_estimators=n_estimators,
-        max_depth=max_depth,
-        random_state=42
-    )
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-
-    mlflow.log_param("n_estimators", n_estimators)
-    mlflow.log_param("max_depth", max_depth)
-    mlflow.log_metric("accuracy", acc)
-
-    # Log model
-    mlflow.sklearn.log_model(model, "random_forest_model")
-
-    print(f"Model logged with accuracy: {acc}")
+for i, element in enumerate(models):
+    model_name = element[0]
+    params = element[1]
+    model = element[2]
+    report = reports[i]
+    
+    with mlflow.start_run(run_name=model_name):        
+        mlflow.log_params(params)
+        mlflow.log_metrics({
+            'accuracy': report['accuracy'],
+            'recall_class_1': report['1']['recall'],
+            'recall_class_0': report['0']['recall'],
+            'f1_score_macro': report['macro avg']['f1-score']
+        })  
+        
+        if "XGB" in model_name:
+            mlflow.xgboost.log_model(model, "model")
+        else:
+            mlflow.sklearn.log_model(model, "model")
